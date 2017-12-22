274b3373da2b71c9b182afd69f38416d4f8b9dcc
daiwenjuan <1581413545@qq.com>
2017-12-22 17:57:06 +0800

    首页

--- src/plugins/home/home.js
+++ src/plugins/home/home.js
@@ -3,17 +3,20 @@
  */
 import React, { PureComponent }from 'react'
 import styles from './home.module.less'
-
+import { Input } from 'antd'
+const Search = Input.Search
 export default class Home extends PureComponent {
 
   render() {
     return (<div className={styles['home-wrapper']}>
       <div className="home-content">
         <div className="header">
+          <span className="logo">LOGO</span>
+          <span className='login'>登录</span>
         </div>
         <div className="middle">
-          <div className="content">
-            MIDDLE
+          <div className="search">
+            <Search placeholder="input search text" enterbutton="Search" size="large"/>
           </div>
         </div>
         <div className="bottom">BOTTOM</div>
