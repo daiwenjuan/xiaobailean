c18803322088ca834df202455d1203f03905a896
daiwenjuan <1581413545@qq.com>
2018-01-12 18:53:27 +0800

    概念记录

--- src/plugins/javascript-dom/javascript_dom.js
+++ src/plugins/javascript-dom/javascript_dom.js
@@ -13,7 +13,21 @@ export default class JavaScriptDom extends Component {
   render() {
     return (
       <div className={styles['wrapper']}>
-        sdfdsfdsfd
+        <header className="title">编码良好规范</header>
+        <ul>
+          <li>
+            <div>循序渐进</div>
+            <div className="info">
+              <span>从最核心的你日荣开始，逐步添加额外的功能。应该先用标记语言给核心内容加上正确的标记以使其获得正确的结构，然后再逐步
+              充实被加上了正确标记的内容。可以通过CSS样式实现，也可以通过DOM脚本添加各种操作行为</span>
+            </div>
+          </li>
+          <li>
+            <div>预留退路</div>
+            <div className="info"><span>当浏览器JavaScript功能被禁用，也能正常工作</span></div>
+          </li>
+          <li>分离javaScript代码</li>
+        </ul>
       </div>
     )
   }
